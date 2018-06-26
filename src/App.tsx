7e831c4401d5c6c2fe34356b2f7845a40774e435
Radu Dunarentu <dunarentu.radu@gmail.com>
2018-06-26 14:23:16 +0300

    (feat): add nicer table for categories

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
 import * as React from 'react';
 // import styled from 'styled-components';
+import 'typeface-roboto'
 import ForumTable from './components/ForumTable';
 import Header from './components/Header';
 
+const theme = createMuiTheme();
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <Header/>
-        <ForumTable />
-      </div>
+      <MuiThemeProvider theme={theme}>
+        <div className="App">
+          <Header/>
+          <ForumTable />
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
--- src/components/ForumTable.tsx
+++ src/components/ForumTable.tsx
@@ -1,20 +1,79 @@
+// import {Theme} from '@material-ui/core';
+// import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Paper from '@material-ui/core/Paper';
+// import createStyles from '@material-ui/core/styles/createStyles';
+// import withStyles from '@material-ui/core/styles/withStyles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import FolderIcon from '@material-ui/icons/Folder';
 import * as React from 'react';
-import { firestore } from '../firebase';
 import '../styles.css';
+
+interface IData {
+    id: number;
+    name: string;
+    date: string;
+}
+
+let id = 0;
+function createData(name: string): IData {
+  id += 1;
+  const date = (new Date).toLocaleString();
+  return { id, name, date};
+}
+
+const data: IData[] = [
+  createData('Frozen yoghurt'),
+  createData('Ice cream sandwich'),
+  createData('Eclair'),
+  createData('Cupcake'),
+  createData('Gingerbread'),
+];
 
 class ForumTable extends React.Component {
 
     render() {
-      firestore.collection("users").get().then((querySnapshot) => {
-          querySnapshot.forEach((doc) => {
-            console.log(doc.data());
-            console.log(doc.id + '=>' + doc.data());
-          });
-        });
         return(
-            <div className='appLayout__constrainWidth'>Forum</div>
+            <div className='appLayout__constrainWidth'>
+              <Paper>
+                <Table>
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>Category</TableCell>
+                      <TableCell>Last Post</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {data.map(n => {
+                      return (
+                        <TableRow key={n.id}>
+                          <TableCell component="th" scope="row">
+                            <ListItem>
+                              <ListItemIcon>
+                                <FolderIcon />
+                              </ListItemIcon>
+                              <ListItemText
+                                primary={n.name}
+                                secondary={'Secondary text'}
+                              />
+                            </ListItem>
+                          </TableCell>
+                          <TableCell>{n.date}</TableCell>
+                        </TableRow>
+                      );
+                    })}
+                  </TableBody>
+                </Table>
+              </Paper>
+            </div>
         );
     }
 }
 
-export default ForumTable;
+export default (ForumTable);
